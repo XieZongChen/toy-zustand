d8264668fd995729e0b5563aab1dac3a788fe308
XieZongChen <952937570@qq.com>
2025-01-20 10:41:49 +0800

    feat: 初始化项目 & 实现一个使用原版 zustand 的 demo

---
+++ README.md
@@ -0,0 +1,3 @@
+# toy-zustand
+
+自己实现一个 zustand，研究一下这个状态库的实现
---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { create } from 'zustand';
+
+const useTestStore = create((set) => ({
+  aaa: '',
+  bbb: '',
+  updateAaa: (value) => set(() => ({ aaa: value })),
+  updateBbb: (value) => set(() => ({ bbb: value })),
+}));
+
+export default function App() {
+  const updateAaa = useTestStore((state) => state.updateAaa);
+  const aaa = useTestStore((state) => state.aaa);
+
+  return (
+    <div>
+      <input onChange={(e) => updateAaa(e.currentTarget.value)} value={aaa} />
+      <Bbb></Bbb>
+    </div>
+  );
+}
+
+function Bbb() {
+  return (
+    <div>
+      <Ccc></Ccc>
+    </div>
+  );
+}
+
+function Ccc() {
+  const aaa = useTestStore((state) => state.aaa);
+  return <p>hello, {aaa}</p>;
+}
