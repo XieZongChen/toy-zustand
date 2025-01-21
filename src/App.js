79f22a3aefbd276ea98d11d455e191531a3f320e
XieZongChen <952937570@qq.com>
2025-01-21 09:54:42 +0800

    feat: 测试 toy-zustand

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
-import { create } from 'zustand';
+// import { create } from 'zustand';
+import { create } from './toy-zustand';
 import { persist } from 'zustand/middleware';
 
 function logMiddleware(func) {
